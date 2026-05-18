bf013be0332072a382c021acf0e6407a9fdcdaeb
Murilo Camargo <murilo.camargozac@gmail.com>
2026-05-18 16:17:44 -0300

    aprendendo switch e exercicio básico

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 80.5     
+console.log(`A velocidade média do seu carro é ${vel} Km/h.`)
+console.log('dirija sempre usando cinto de segurança')
+
+if (vel > 80){
+ console.log('Voce está acima da velocidade permitida por lei. MULTADO')   
+}
